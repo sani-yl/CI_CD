53797a6ffbc0fbbf8d711533273b3bce73908a8d
Matti Luukkainen <mluukkai@iki.fi>
2026-03-12 12:04:21 +0200

    Change server port to 5001 and refactor start method

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require("express");
 const app = express();
 
 // get the port from env variable
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.use(express.static("dist"));
 
-app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
-});
+const start = async () => {
+  await app.listen(PORT)
+  console.log(`server started on port ${PORT}`)
+}
+
+start()
